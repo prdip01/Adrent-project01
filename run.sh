7d67d21a1e6431d560d2a639a4f19547edffca8a
prdip01 <iamprdip160@gmail.com>
2025-07-03 13:05:08 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Launching Streamlit Spam Detection App..."
+streamlit run main.py
---
+++ run_me.py
@@ -0,0 +1,4 @@
+import os
+
+print("Launching Streamlit Spam Detection App...")
+os.system("streamlit run main.py")
